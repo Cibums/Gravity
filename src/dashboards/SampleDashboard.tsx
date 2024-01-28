d2e1eaa98fd25b39f651f12a946e37cdf5c0b44d
Cibums <lucas.fransson@hotmail.com>
2024-01-29 00:16:52 +0100

    Add Fluent UI dropdown and stack components

--- src/dashboards/SampleDashboard.tsx
+++ src/dashboards/SampleDashboard.tsx
@@ -1,15 +1,32 @@
 import { BaseDashboard } from "@microsoft/teamsfx-react";
+import { Dropdown } from '@fluentui/react/lib/Dropdown';
 
 import ChartWidget from "../widgets/ChartWidget";
 import ListWidget from "../widgets/ListWidget";
+import { Stack } from "@fluentui/react";
 
 export default class SampleDashboard extends BaseDashboard<any, any> {
+  private dropdownOptions = [
+    { key: 'option1', text: 'Option 1' },
+    { key: 'option2', text: 'Option 2' },
+    { key: 'option3', text: 'Option 3' },
+    // Add more options as needed
+  ];
+
   override layout(): JSX.Element | undefined {
     return (
-      <>
-        <ListWidget />
-        <ChartWidget />
-      </>
+      <Stack>
+        <Dropdown
+          label="Choose an option:"
+          placeholder="Select an option"
+          options={this.dropdownOptions}
+          styles={{ root: { width: 300 } }}
+        />
+        <Stack tokens={{ childrenGap: 20 }} styles={{ root: { width: '100%' } }}>
+          <ListWidget />
+          <ChartWidget />
+        </Stack>
+      </Stack>
     );
   }
 }
